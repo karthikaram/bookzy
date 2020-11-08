9bb43cf0e8303bd940b636297f863a0a93483818
karthikaram <karthikaram2010@gmail.com>
2020-11-08 13:48:34 -0500

    Updates to trigger pipeline

--- src/main/java/com/bookzy/webapp/WebappApplication.java
+++ src/main/java/com/bookzy/webapp/WebappApplication.java
@@ -1,5 +1,5 @@
 package com.bookzy.webapp;
-//let's trigger  pipeline
+//Triggering pipeline
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
